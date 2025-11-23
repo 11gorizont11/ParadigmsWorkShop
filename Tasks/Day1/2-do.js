4c97196ab32f8274d23a0fa2daf12434da88a918
rhordiichuk <rhordiichuk@gmail.com>
2025-11-23 12:57:34 +0100

    This reverts commit 351f1ef (Hordiichuk Roman: completed 4 tasks from day 1, 2025-11-22).

--- Tasks/Day1/2-do.js
+++ Tasks/Day1/2-do.js
@@ -1,29 +1,8 @@
 'use strict';
 
-class Do {
-  #personData;
+// Put implementation here
 
-  constructor (id) {
-    this.#personData = {id: this.id, name: 'marcus', age: 42};
-  }
-
-  nameMatch(name) {
-    return name === 'marcus';
-  }
-
-  log(age) {
-    console.log(age)
-  }
-
-  run() {
-    return this.nameMatch(this.#personData.name) ? this.log(this.#personData.age) : {};
-  }
-}
-
-// Do({ id: 15 })
-//   .bind(({ id }) => ({ id, name: 'marcus', age: 42 }))
-//   .bind(({ name, age }) => (name === 'marcus' ? (log) => log(age) : () => {}))
-//   .run()(console.log);
-
-
-const rewritedDo = new Do({id: 15}).run();
+Do({ id: 15 })
+  .bind(({ id }) => ({ id, name: 'marcus', age: 42 }))
+  .bind(({ name, age }) => (name === 'marcus' ? (log) => log(age) : () => {}))
+  .run()(console.log);
--- Tasks/Day1/3-execute.js
+++ Tasks/Day1/3-execute.js
@@ -1,92 +1,33 @@
-"use strict";
+'use strict';
 
-// const reader = ({ id }) => ({ id, name: "marcus", age: 42 });
+const reader = ({ id }) => ({ id, name: 'marcus', age: 42 });
 
-// const execute =
-//   (plan) =>
-//   (reader, log, env = {}) => {
-//     if (plan.read) {
-//       const user = reader(plan.read);
-//       return execute(plan.then)(reader, log, { user });
-//     }
-//     if (plan.match) {
-//       const ok = env.user.name === plan.match.name;
-//       return execute(ok ? plan.success : plan.fail)(reader, log, env);
-//     }
-//     if (plan.effect) {
-//       if (plan.effect.log) return () => log(env.user[plan.effect.log]);
-//       if (plan.effect === "noop") return () => {};
-//     }
-//   };
-
-// execute({
-//   read: { id: 15 },
-//   then: {
-//     match: { name: "marcus" },
-//     success: { effect: { log: "age" } },
-//     fail: { effect: "noop" },
-//   },
-// })(reader, console.log)();
-
-// --------------------------------------------------------------------
-
-class Reader {
-  #queryOptions;
-
-  constructor(queryOptions) {
-    this.#queryOptions = queryOptions;
+const execute = (plan) => (reader, log, env = {}) => {
+  if (plan.read) {
+    const user = reader(plan.read);
+    return execute(plan.then)(reader, log, { user });
   }
-
-  readData() {
-    return { name: "marcus", age: 42, ...this.#queryOptions };
+  if (plan.match) {
+    const ok = env.user.name === plan.match.name;
+    return execute(ok ? plan.success : plan.fail)(reader, log, env);
   }
-}
-
-class Exec {
-  #options;
-
-  constructor(options) {
-    this.#options = { ...options };
+  if (plan.effect) {
+    if (plan.effect.log) return () => log(env.user[plan.effect.log]);
+    if (plan.effect === 'noop') return () => {};
   }
+};
 
-  run(steps) {
-    this.#executeAllSteps(steps);
-
-    if (steps.length === 0 || !steps.every((s) => s.satisfied))
-      this.#options.results.failed();
-    this.#options.results.success(console.log, this.#options.data.age);
-  }
-
-  #executeAllSteps(steps) {
-    for (const step of steps) {
-      const result = step.action();
-      step.satisfied = !!result;
-    }
-  }
-}
-
-const options = Object.freeze({
-  data: new Reader({id: 15}).readData(),
-  results: {
-    success(log, value) {
-      log(value);
-    },
-    failed: () => {},
+execute({
+  read: { id: 15 },
+  then: {
+    match: { name: 'marcus' },
+    success: { effect: { log: 'age' } },
+    fail: { effect: 'noop' },
   },
-});
-
-const steps = [
-  {
-    name: "match",
-    action(name) {
-      return name === "marcus";
-    },
-    satisfied: false,
-  },
-];
-
-const main = new Exec(options);
-main.run(steps);
+})(reader, console.log)();
 
 // 1. Rewrite in OOP style
 // 2. Improve data structure inconsistence
+
+// const main = new Exec(options);
+// main.run(steps);
